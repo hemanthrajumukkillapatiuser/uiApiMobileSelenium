1db3cd265f3fd8a3fac3522eeb5bb363a2cca1b1
hemanthrajumukkillapatiuser <hemanthraju.m1991@gmail.com>
2026-05-09 17:49:53 -0400

    Added ConfigReader and FrameworkConstants

---
+++ src/main/java/com/hemanth/automation/constants/FrameworkConstants.java
@@ -0,0 +1,11 @@
+package com.hemanth.automation.constants;
+
+public final class FrameworkConstants {
+
+    private FrameworkConstants() {
+    }
+
+    public static final String CONFIG_FILE_PATH = "src/main/resources/config.properties";
+    public static final String SCREENSHOT_PATH = "target/screenshots/";
+    public static final String ALLURE_RESULTS_PATH = "target/allure-results/";
+}
